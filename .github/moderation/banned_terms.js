cbb0f552f2ed27d38c947b457904bd919a7a22db
MachieCodes <rickytheracc@gmail.com>
2025-04-25 19:59:12 -0500

    Start

---
+++ .github/moderation/banned_terms.js
@@ -0,0 +1,40 @@
+
+
+const clientTerms = [
+    "feather client",
+    "lunar client",
+    "labymod",
+    "tlauncher",
+    "pojav",
+    "boze",
+    "liquid bounce",
+    "mio client",
+    "future client"
+]
+
+const anticheatTerms = [
+    "bypass",
+    "ncp",
+    "nocheat plus",
+    "vulcan",
+    "grim",
+    "wraith",
+    "antiaura",
+    "anticheataddition",
+    "godseye"
+]
+
+const featureTerms = [
+    "infinite reach",
+    "godmode",
+    "portal godmode",
+    "dupe",
+    "ping bypass",
+    "tp aura",
+    "force op"
+]
+
+async function run() {
+
+}
+
