9e961a771e0f728e95dbe20ccebf0127060826c7
MachieCodes <rickytheracc@gmail.com>
2025-04-25 20:39:28 -0500

    Banned Terms Check

--- .github/moderation/banned_terms.js
+++ .github/moderation/banned_terms.js
@@ -1,4 +1,5 @@
-
+const github = require('@actions/github');
+const core = require('@actions/core');
 
 const clientTerms = [
     "feather client",
@@ -10,7 +11,7 @@ const clientTerms = [
     "liquid bounce",
     "mio client",
     "future client"
-]
+];
 
 const anticheatTerms = [
     "bypass",
@@ -21,8 +22,9 @@ const anticheatTerms = [
     "wraith",
     "antiaura",
     "anticheataddition",
-    "godseye"
-]
+    "godseye",
+    "anti xray"
+];
 
 const featureTerms = [
     "infinite reach",
@@ -31,10 +33,93 @@ const featureTerms = [
     "dupe",
     "ping bypass",
     "tp aura",
-    "force op"
-]
+    "force op",
+    ".panic"
+];
+
+const token = process.env.GITHUB_TOKEN;
+const octokit = github.getOctokit(token);
+const context = github.context
+
+const issueNumber = context.payload.issue.number;
+const owner = context.repo.owner;
+const repo = context.repo.repo;
+const title = context.payload.issue.title;
+const body = context.payload.issue.body;
 
 async function run() {
+    const issueText = `${title} ${body}`.toLowerCase();
 
+    for (const term of clientTerms) {
+        if (!issueText.includes(term)) continue;
+
+        const clientMessage = `This issue is being automatically closed because it may mention a third-party client 
+        (${term}). Meteor only supports Fabric via Prism Launcher and the Vanilla Launcher. Any problems you encounter
+        while using other clients are your responsibility to troubleshoot.\n
+        \n 
+        If you believe this issue was closed wrongly, you may reopen it.`
+
+        await closeIssue(term, clientMessage);
+        return;
+    }
+
+    for (const term of anticheatTerms) {
+        if (!issueText.includes(term)) continue;
+
+        const anticheatMessage = `This issue is being automatically closed because it may mention issues with 
+        anticheat plugins (${term}). Meteor is intended to be used only as a utility client on servers that 
+        allow it, we do not intend to add workarounds for specific anticheats unless it falls in that scope.\n
+        \n
+        If you believe this issue was closed wrongly, you may reopen it.`;
+
+        await closeIssue(term, anticheatMessage);
+        return;
+    }
+
+    for (const term of featureTerms) {
+        if (!issueText.includes(term)) continue;
+
+        const featureMessage = `This issue is being automatically closed because it may mention features that are
+        impossible to make, associated with cheating/griefing, or fall outside the scope of this project (${term}).\n
+        \n
+        If you believe this issue was closed wrongly, you may reopen it.`;
+
+        await closeIssue(term, featureMessage);
+        return;
+    }
 }
+
+async function closeIssue(foundTerm, message) {
+    core.warning(`Found banned term ('${foundTerm}'), closing issue.`);
+
+    // IDE for whatever reason can't find the rest property yippee
+
+    try {
+        await octokit["rest"].issues.createComment({
+            owner,
+            repo,
+            issue_number: issueNumber,
+            body: message,
+        });
+
+        core.info('Comment added successfully.');
+    } catch (error) {
+        core.error(`Failed to add comment: ${error.message}`);
+    }
+
+    try {
+        await octokit["rest"].issues.update({
+            owner,
+            repo,
+            issue_number: issueNumber,
+            state: 'closed',
+        });
+
+        core.info('Closed issue successfully.');
+    } catch (error) {
+        core.setFailed(`Failed to close issue: ${error.message}`);
+    }
+}
+
+run();
 
